41aadc76a1354bbf09d52ba2c0397d4563fe0047
BInh Nguyen <csbinhnguyen.108@gmail.com>
2017-09-12 10:33:36 -0400

    Pygame test Press space to change the square's color Issue: cannot move square with arrow keys

--- Class1.py
+++ Class1.py
@@ -1,6 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
+import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((400,300))
+done = False
+is_blue = True
+x = 30
+y = 30
+
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
+
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= 3
+    if pressed[pygame.K_DOWN]:
+        y += 3
+    if pressed[pygame.K_LEFT]:
+        x -= 3
+    if pressed[pygame.K_RIGHT]:
+        x += 3
+
+    screen.fill((0, 0, 0))
+    if is_blue:
+        color = (0, 128, 255)
+    else:
+        color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+'''Tkinter Test
 class App:
     def __init__(self,master):
         def onClick():
@@ -12,7 +50,7 @@ class App:
         self.button = Button(frame, text="Quit", fg="red", command=frame.quit)
         self.button.pack(side=LEFT)
 
-        self.hi = Button(frame, text="Hello", fg="blue", command=onClick)
+        self.hi = Button(frame, text="Hello", fg="green", command=onClick)
         self.hi.pack(side=LEFT)
 
 root = Tk()
@@ -20,8 +58,8 @@ root = Tk()
 app = App(root)
 
 root.mainloop()
-
 '''
+''' Calculator
 # Add two numbers
 def add(num1, num2):
     return num1 + num2
